6eb8b9abee9b34f1c3854fe79a3273e4a5a862a8
JoelEspinos <jespinosruiz@gmail.com>
2025-11-07 00:30:43 +0100

    Començar amb MusicList
    S'ha començat el desenvolupament del component MusicList, de moment hi ha el Cercador, La llista de cançons i el botó de obrir el formulari, també s'ha fet la primera comunicació entre MusicList i App.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { Musiclist } from './view/musiclist/musiclist';
 import { Player } from './view/player/player';
 import { Addform } from './view/addform/addform';
@@ -10,5 +10,17 @@ import { Addform } from './view/addform/addform';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('spotidaw-music-player');
+  private _viewMode: WritableSignal<string>;
+
+  constructor() {
+    this._viewMode = signal<string>("General");
+  }
+
+  public get viewMode(): Signal<string> {
+    return this._viewMode.asReadonly();
+  }
+
+  public switchViewMode(mode: string) {
+    this._viewMode.set(mode);
+  }
 }
--- src/app/view/musiclist/musiclist.ts
+++ src/app/view/musiclist/musiclist.ts
@@ -1,18 +1,56 @@
-import { Component, signal, WritableSignal } from '@angular/core';
-import { SONGS } from '../../model/songs'
+import { Component, computed, output, OutputEmitterRef, Signal, signal, WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { SONGS } from '../../model/songs';
 
 @Component({
   selector: 'app-musiclist',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './musiclist.html',
   styleUrl: './musiclist.css',
 })
 
 export class Musiclist {
   private _songsList: WritableSignal<any[]>;
+  private _filteredSongsList: Signal<any[]>;
+  private _searchSong: WritableSignal<string>;
+  public _changeViewMode: OutputEmitterRef<string> = output();
 
   constructor() {
-    this._songsList = signal<any[]>(SONGS);
+   let storedSongsList = localStorage.getItem("STORED_SONGS");
+   
+   if (storedSongsList === null) this._songsList = signal<any[]>(SONGS);
+   else this._songsList = signal<any[]>(JSON.parse(storedSongsList));
+
+   this._filteredSongsList = computed<any[]>(() => {
+    let filteredList: any[] = [];
+
+    if (this._searchSong().length > 0) {
+      for (let i = 0; i < this._songsList().length; i++) {
+        if (this._songsList()[i].title.includes(this._searchSong()) || this._songsList()[i].artist.includes(this._searchSong())) {
+          filteredList.push(this._songsList()[i]);
+        }
+      }
+    }
+
+    return filteredList;
+   });
+   this._searchSong = signal<string>("");
+  }
+
+  public get songsList(): Signal<any[]> {
+    return this._songsList.asReadonly();
+  }
+
+  public get filteredSongsList(): Signal<any> {
+    return this._filteredSongsList;
+  }
+
+  public get searchSong(): WritableSignal<string> {
+    return this._searchSong;
+  }
+
+  public changeViewModeToAddForm(): void {
+    this._changeViewMode.emit("AddForm");
   }
 
 }
