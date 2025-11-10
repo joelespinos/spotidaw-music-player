96c9724f02ba2fa4885c9436efb52e745897b825
JoelEspinos <jespinosruiz@gmail.com>
2025-11-10 02:43:47 +0100

    Començar amb AddForm + Estils
    S'ha començat amb el component AddForm i s'han acabat de perfilar alguns estils pendents de App i de MusicList

--- src/app/view/addform/addform.ts
+++ src/app/view/addform/addform.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,43 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './addform.html',
   styleUrl: './addform.css',
 })
-export class Addform {
 
+export class Addform {
+  private _title: WritableSignal<string>;
+  private _artist: WritableSignal<string>;
+  private _mp3Url: WritableSignal<string>;
+  private _cover: WritableSignal<string>;
+  private _description: WritableSignal<string>;
+
+  constructor() {
+    this._title = signal<string>("");
+    this._artist = signal<string>("");
+    this._mp3Url = signal<string>("");
+    this._cover = signal<string>("");
+    this._description = signal<string>("");
+  }
+
+  public get title(): WritableSignal<string> {
+    return this._title;
+  }
+
+  public get artist(): WritableSignal<string> {
+    return this._artist;
+  }
+
+  public get mp3Url(): WritableSignal<string> {
+    return this._mp3Url;
+  }
+
+  public get cover(): WritableSignal<string> {
+    return this._cover;
+  }
+
+  public get description(): WritableSignal<string> {
+    return this._description;
+  }
+
+  public mirar(): void{
+    console.log(this._title);
+  }
 }
